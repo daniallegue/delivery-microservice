8a0d6cf07b5492d4bc6425200a4b4602d663806c
Rares Burghelea <R.Burghelea@student.tudelft.nl>
2023-12-05 18:19:38 +0100

    Create basic delivery controller

---
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controller/DeliveryController.java
@@ -0,0 +1,8 @@
+package nl.tudelft.sem.template.example.controller;
+
+import nl.tudelft.sem.template.api.DeliveryApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeliveryController implements DeliveryApi {
+}
