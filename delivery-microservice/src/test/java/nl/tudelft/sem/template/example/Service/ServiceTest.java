48a6349263747def131b35cd392bf995b69f4597
Razvan Gherasa <R.M.Gherasa@student.tudelft.nl>
2023-12-13 16:37:32 +0200

    comments

--- delivery-microservice/src/test/java/nl/tudelft/sem/template/example/Service/ServiceTest.java
+++ delivery-microservice/src/test/java/nl/tudelft/sem/template/example/Service/ServiceTest.java
@@ -42,24 +42,19 @@ public class ServiceTest {
 
     @Test
     public void getOrderStatus_existingOrder_returnsStatus() throws OrderNotFoundException {
-        // Arrange
         Order mockOrder = new Order();
         mockOrder.setStatus(Order.StatusEnum.PENDING);
         when(orderRepository.findById(anyLong())).thenReturn(Optional.of(mockOrder));
 
-        // Act
         Order.StatusEnum status = orderService.getOrderStatus(1);
 
-        // Assert
         assertThat(status).isEqualTo(Order.StatusEnum.PENDING);
     }
 
     @Test
     public void getOrderStatus_nonExistingOrder_throwsException() {
-        // Arrange
         when(orderRepository.findById(anyLong())).thenReturn(Optional.empty());
 
-        // Act & Assert
         assertThatThrownBy(() -> orderService.getOrderStatus(1))
                 .isInstanceOf(OrderNotFoundException.class)
                 .hasMessageContaining("Order id not found");
@@ -67,102 +62,82 @@ public class ServiceTest {
 
     @Test
     public void setOrderStatus_pendingToAccepted_validTransition() throws Exception {
-        // Arrange
         Order mockOrder = new Order();
         mockOrder.setStatus(Order.StatusEnum.PENDING);
         when(orderRepository.findById(anyLong())).thenReturn(Optional.of(mockOrder));
 
-        // Act
         orderService.setOrderStatus(1, "Accepted");
 
-        // Assert
         assertThat(mockOrder.getStatus()).isEqualTo(Order.StatusEnum.ACCEPTED);
         verify(orderRepository).save(mockOrder);
     }
 
     @Test
     public void setOrderStatus_pendingToRejected_validTransition() throws Exception {
-        // Arrange
         Order mockOrder = new Order();
         mockOrder.setStatus(Order.StatusEnum.PENDING);
         when(orderRepository.findById(anyLong())).thenReturn(Optional.of(mockOrder));
 
-        // Act
         orderService.setOrderStatus(1, "Rejected");
 
-        // Assert
         assertThat(mockOrder.getStatus()).isEqualTo(Order.StatusEnum.REJECTED);
         verify(orderRepository).save(mockOrder);
     }
 
     @Test
     public void setOrderStatus_acceptedToPreparing_validTransition() throws Exception {
-        // Arrange
         Order mockOrder = new Order();
         mockOrder.setStatus(Order.StatusEnum.ACCEPTED);
         when(orderRepository.findById(anyLong())).thenReturn(Optional.of(mockOrder));
 
-        // Act
         orderService.setOrderStatus(1, "Preparing");
 
-        // Assert
         assertThat(mockOrder.getStatus()).isEqualTo(Order.StatusEnum.PREPARING);
         verify(orderRepository).save(mockOrder);
     }
 
     @Test
     public void setOrderStatus_preparingToGivenToCourier_validTransition() throws Exception {
-        // Arrange
         Order mockOrder = new Order();
         mockOrder.setStatus(Order.StatusEnum.PREPARING);
         when(orderRepository.findById(anyLong())).thenReturn(Optional.of(mockOrder));
 
-        // Act
         orderService.setOrderStatus(1, "Given_To_Courier");
 
-        // Assert
         assertThat(mockOrder.getStatus()).isEqualTo(Order.StatusEnum.GIVEN_TO_COURIER);
         verify(orderRepository).save(mockOrder);
     }
 
     @Test
     public void setOrderStatus_givenToCourierToOnTransit_validTransition() throws Exception {
-        // Arrange
         Order mockOrder = new Order();
         mockOrder.setStatus(Order.StatusEnum.GIVEN_TO_COURIER);
         when(orderRepository.findById(anyLong())).thenReturn(Optional.of(mockOrder));
 
-        // Act
         orderService.setOrderStatus(1, "On_Transit");
 
-        // Assert
         assertThat(mockOrder.getStatus()).isEqualTo(Order.StatusEnum.ON_TRANSIT);
         verify(orderRepository).save(mockOrder);
     }
 
     @Test
     public void setOrderStatus_onTransitToDelivered_validTransition() throws Exception {
-        // Arrange
         Order mockOrder = new Order();
         mockOrder.setStatus(Order.StatusEnum.ON_TRANSIT);
         when(orderRepository.findById(anyLong())).thenReturn(Optional.of(mockOrder));
 
-        // Act
         orderService.setOrderStatus(1, "Delivered");
 
-        // Assert
         assertThat(mockOrder.getStatus()).isEqualTo(Order.StatusEnum.DELIVERED);
         verify(orderRepository).save(mockOrder);
     }
 
     @Test
     public void setOrderStatus_invalidTransition_throwsException() {
-        // Arrange
         Order mockOrder = new Order();
         mockOrder.setStatus(Order.StatusEnum.PENDING);
         when(orderRepository.findById(anyLong())).thenReturn(Optional.of(mockOrder));
 
-        // Act & Assert
         assertThatThrownBy(() -> orderService.setOrderStatus(1, "Preparing"))
                 .isInstanceOf(IllegalOrderStatusException.class)
                 .hasMessageContaining("Error! Order status cant go from PENDING to PREPARING.");
